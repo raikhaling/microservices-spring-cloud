03205a2bbdcd3a44a055d188383e32dadb811c78
nabin rai <rainabinkhyal@gmail.com>
2023-10-04 08:22:46 +0545

    feign proxy intregation

--- currency-conversion-service/src/main/java/com/khaling/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/khaling/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.khaling.microservices.currencyconversionservice.controller;
 
 import com.khaling.microservices.currencyconversionservice.entity.CurrencyConversion;
+import com.khaling.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,10 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 @RestController
+@RequiredArgsConstructor
 public class CurrencyConversionController {
 
-
+    private final CurrencyExchangeProxy exchangeProxy;
 
 
 
@@ -37,6 +40,23 @@ public class CurrencyConversionController {
         return  new CurrencyConversion(currencyConversion.getId(), from, to,
                 currencyConversion.getConversionMultiple(), quantity,
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment()+" rest template");
+    }
+
+    //using feign
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ){
+
+
+        CurrencyConversion currencyConversion = exchangeProxy.retrieveExchangeValue(from, to);
+
+        return  new CurrencyConversion(currencyConversion.getId(), from, to,
+                currencyConversion.getConversionMultiple(), quantity,
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()+ " feign");
     }
 }
---
+++ currency-conversion-service/src/main/java/com/khaling/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.khaling.microservices.currencyconversionservice.proxy;
+
+import com.khaling.microservices.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to
+    );
+}
